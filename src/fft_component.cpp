d16b6d2666041b555518ca106424b0339b8dde64
yoooomaruuuu <yoooomaruuuu@gmail.com>
2020-10-25 15:42:21 +0900

    クラス関数を呼び出すことが難しいため、呼び出し関数を作成予定

--- src/fft_component.cpp
+++ src/fft_component.cpp
@@ -2,27 +2,35 @@
 
 using namespace audio_analysis_lib;
 
-DLLEXPORT fft_component::fft_component(int fft_size)
+fft_component::fft_component(int fft_size)
 	:pImpl(std::make_unique<fft_component_impl>(fft_size)) { }
 
-DLLEXPORT fft_component::~fft_component() {};
+fft_component::~fft_component() {};
 
-DLLEXPORT fft_component::fft_exception fft_component::fft(const float* input_re, const float* input_im, float* output_re, float* output_im)
+fft_component::fft_exception fft_component::fft(const float* input_re, const float* input_im, float* output_re, float* output_im)
 {
 	return pImpl->fft(input_re, input_im, output_re, output_im);
 }
 
-DLLEXPORT fft_component::fft_exception fft_component::ifft(const float* input_re, const float* input_im, float* output_re, float* output_im)
+fft_component::fft_exception fft_component::ifft(const float* input_re, const float* input_im, float* output_re, float* output_im)
 {
 	return pImpl->ifft(input_re, input_im, output_re, output_im);
 }
 
-DLLEXPORT fft_component::fft_exception fft_component::fft_mode_setting(fft_mode mode)
+fft_component::fft_exception fft_component::fft_mode_setting(fft_mode mode)
 {
 	return pImpl->fft_mode_setting(mode);
 }
 
-DLLEXPORT int fft_component::get_fft_size()
+int fft_component::get_fft_size()
 {
 	return pImpl->get_fft_size();
+}
+
+
+//あれ？マーシャリングのダブルポインタ運用ってできたっけ
+void init_fft_component(int init_fft_size, void** func_object)
+{
+	fft_component* tmp = new fft_component(init_fft_size);
+	*func_object = static_cast<void*>(tmp);
 }
--- src/fft_component.hpp
+++ src/fft_component.hpp
@@ -5,7 +5,7 @@
 extern "C"
 #endif
 
-#define DLLEXPORT __declspec(dllexport)
+#define DLLEXPORT __declspec(dllexport) extern "C"
 
 namespace audio_analysis_lib
 {
@@ -18,20 +18,32 @@ namespace audio_analysis_lib
 		using fft_mode = fft_component_impl::fft_mode;
 		using fft_exception = fft_component_impl::fft_exception;
 
-		DLLEXPORT fft_component(int fft_size);
+		fft_component(int fft_size);
 
-		DLLEXPORT fft_exception fft(const float* input_re, const float* input_im, float* output_re, float* output_im);
+		fft_exception fft(const float* input_re, const float* input_im, float* output_re, float* output_im);
 
-		DLLEXPORT fft_exception ifft(const float* input_re, const float* input_im, float* output_re, float* output_im);
+		fft_exception ifft(const float* input_re, const float* input_im, float* output_re, float* output_im);
 
-		DLLEXPORT fft_exception fft_mode_setting(fft_mode mode);
+		fft_exception fft_mode_setting(fft_mode mode);
 
-		DLLEXPORT int get_fft_size();
+		int get_fft_size();
 
-		DLLEXPORT ~fft_component();
+		~fft_component();
 
 	private:
 		std::unique_ptr<fft_component_impl> pImpl;
 	};
 
+	DLLEXPORT void init_fft_component(int init_fft_size, void* func_object);
+
+	DLLEXPORT fft_component::fft_exception fft(const float* input_re, const float* input_im, float* output_re, float* output_im, void* func_object);
+
+	DLLEXPORT fft_component::fft_exception ifft(const float* input_re, const float* input_im, float* output_re, float* output_im, void* func_object);
+
+	DLLEXPORT fft_component::fft_exception fft_mode_setting(fft_component::fft_mode mode, void* func_object);
+
+	DLLEXPORT int get_fft_size(void* func_object);
+
+	DLLEXPORT void delete_fft_component(void* func_object);
 }
+
