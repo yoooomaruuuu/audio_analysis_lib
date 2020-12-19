ecd1c8bf54a283bb8bdc12a79a4ede7fa4c2d193
yoooomaruuuu <yoooomaruuuu@gmail.com>
2020-12-19 10:04:39 +0900

    一部コメント削除

--- src/world_d4c_ap.cpp
+++ src/world_d4c_ap.cpp
@@ -6,7 +6,7 @@ d4c_ap::d4c_ap(int init_fs)
 	: pimpl(std::make_unique<my_world_voice_feature::d4c_ap_impl>(init_fs))
 { }
 
-void d4c_ap::spectrogram_estimated(const double *x, int x_length, const double *temporal_position, const double *f0, int f0_length, int fft_size, double **aperiodicity)
+void d4c_ap::aperiodicity_estimated(const double *x, int x_length, const double *temporal_position, const double *f0, int f0_length, int fft_size, double **aperiodicity)
 {
 	pimpl->aperiodicity_estimated(x, x_length, temporal_position, f0, f0_length, fft_size, aperiodicity);
 }
--- src/world_d4c_ap.hpp
+++ src/world_d4c_ap.hpp
@@ -11,7 +11,7 @@ namespace audio_analysis_lib
 	{
 	public:
 		d4c_ap(int init_fs);
-		void spectrogram_estimated(const double *x, int x_length, const double *temporal_position, const double *f0, int f0_length, int fft_size, double **aperiodicity);
+		void aperiodicity_estimated(const double *x, int x_length, const double *temporal_position, const double *f0, int f0_length, int fft_size, double **aperiodicity);
 	private:
 		std::unique_ptr<my_world_voice_feature::d4c_ap_impl> pimpl;
 	};
