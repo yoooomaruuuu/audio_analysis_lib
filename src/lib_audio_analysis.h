90a462ae32d8035f5d11a8e54cb5fcc563df2ee3
yoooomaruuuu <yoooomaruuuu@gmail.com>
2020-12-19 10:06:14 +0900

    world音声合成機能追加!

--- src/lib_audio_analysis.h
+++ src/lib_audio_analysis.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "fft_component.hpp"
 #include "window_functions.hpp"
+#include "world_harvest_f0.hpp"
+#include "world_cheaptrick_sp.hpp"
+#include "world_d4c_ap.hpp"
+#include "world_synthesis.hpp"
 #ifdef _WIN32
 #include "input_capture.hpp"
 #endif
---
+++ src/world_synthesis.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "world_synthesis_impl.hpp"
+#include <memory>
+
+namespace audio_analysis_lib
+{
+	using namespace my_world_voice_feature;
+
+	class world_synthesis_impl;
+	struct world_parameters;
+
+	class world_synthesis
+	{
+	public:
+		world_synthesis(int init_fs, float init_frame_period);
+		~world_synthesis();
+		void realtime_synth(double f0_shift, double* frame_data, double* output_data);
+	private:
+		std::unique_ptr<my_world_voice_feature::world_synthesis_impl> pimpl;
+	};
+}
+
