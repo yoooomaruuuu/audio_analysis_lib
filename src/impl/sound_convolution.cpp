a892bb71c808e6bda5ba0e35b164a44fe66e179a
tochinai <yoooomaruuuu@gmail.com>
2022-06-02 08:55:50 +0900

    前方宣言系の参照修正

--- src/impl/sound_convolution.cpp
+++ src/impl/sound_convolution.cpp
@@ -1,13 +1,46 @@
 #include "sound_convolution.hpp"
+#include "fft_component.hpp"
 
-audio_analysis_lib::sound_convolution::sound_convolution(int init_fft_size)
-	:pImpl(std::make_unique<sound_convolution_impl>(init_fft_size))
+namespace audio_analysis_lib
 {
-}
+	namespace my_signal_processing
+	{
+		class sound_convolution_impl
+		{
+		public:
+			sound_convolution_impl(int init_fft_size);
+			~sound_convolution_impl();
+			void frame_convolution(const float* src1, const float* src2);
 
-audio_analysis_lib::sound_convolution::~sound_convolution() { }
+		private:
+			float* m_s1_input_re, m_s1_input_im, m_s2_input_re, m_s2_input_im;
+			float* m_s1_output_re, m_s1_output_im, m_s2_output_re, m_s2_output_im;
+			float* m_conv_output;
+		};
 
-void audio_analysis_lib::sound_convolution::frame_convolution(const float* src1, const float* src2)
-{
-	pImpl->frame_convolution(src1, src2);
+		sound_convolution_impl::sound_convolution_impl(int init_fft_size)
+		{
+		}
+
+		sound_convolution_impl::~sound_convolution_impl()
+		{
+
+		}
+
+		void sound_convolution_impl::frame_convolution(const float* src1, const float* src2)
+		{
+
+		}
+	}
+	sound_convolution::sound_convolution(int init_fft_size)
+		:pImpl(std::make_unique<my_signal_processing::sound_convolution_impl>(init_fft_size))
+	{
+	}
+
+	sound_convolution::~sound_convolution() { }
+
+	void sound_convolution::frame_convolution(const float* src1, const float* src2)
+	{
+		pImpl->frame_convolution(src1, src2);
+	}
 }
--- src/sound_convolution.hpp
+++ src/sound_convolution.hpp
@@ -1,13 +1,17 @@
 #pragma once
 #include <iostream>
 #include <memory>
-#include "impl/sound_convolution_impl.hpp"
-#include "fft_component.hpp"
 
 namespace audio_analysis_lib
 {
-	class sound_convolution_impl;
-	class fft_component;
+	namespace my_signal_processing
+	{
+		class sound_convolution_impl;
+	}
+	namespace my_world_voice_feature
+	{
+		class fft_component;
+	}
 
 	class sound_convolution
 	{
@@ -17,6 +21,6 @@ namespace audio_analysis_lib
 		void frame_convolution(const float* src1, const float* src2);
 
 	private:
-		std::unique_ptr<sound_convolution_impl> pImpl;
+		std::unique_ptr<my_signal_processing::sound_convolution_impl> pImpl;
 	};
 }
