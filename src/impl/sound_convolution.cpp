18f6647bf8033cc99d24beb079fd67767b3e61d5
tochinai <yoooomaruuuu@gmail.com>
2022-06-02 09:30:22 +0900

    初期化処理追加

--- src/impl/sound_convolution.cpp
+++ src/impl/sound_convolution.cpp
@@ -1,24 +1,40 @@
 #include "sound_convolution.hpp"
 #include "fft_component.hpp"
+#include "fft_exception.hpp"
 
 namespace audio_analysis_lib
 {
 	namespace my_signal_processing
 	{
+		using namespace my_world_voice_feature;
 		class sound_convolution_impl
 		{
 		public:
-			sound_convolution_impl(int init_fft_size);
+			sound_convolution_impl(int init_frame_size);
 			~sound_convolution_impl();
-			void frame_convolution(const float* src1, const float* src2);
+			float* frame_convolution(const float* src1, const float* src2);
 
 		private:
-			float* m_s1_input_re, m_s1_input_im, m_s2_input_re, m_s2_input_im;
-			float* m_s1_output_re, m_s1_output_im, m_s2_output_re, m_s2_output_im;
-			float* m_conv_output;
+			int m_frame_size, m_fft_size;
+			float *m_s1_input_re, *m_s1_input_im, *m_s2_input_re, *m_s2_input_im;
+			float *m_s1_output_re, *m_s1_output_im, *m_s2_output_re, *m_s2_output_im;
+			float *m_conv_output;
+			std::unique_ptr<fft_component> _fft;
 		};
 
-		sound_convolution_impl::sound_convolution_impl(int init_fft_size)
+		sound_convolution_impl::sound_convolution_impl(int init_frame_size)
+			:m_frame_size(init_frame_size),
+			 m_fft_size(init_frame_size * 2 - 1),
+			 m_s1_input_re((float*)malloc(sizeof(float) * m_fft_size)),
+			 m_s1_input_im((float*)malloc(sizeof(float) * m_fft_size)),
+			 m_s2_input_re((float*)malloc(sizeof(float) * m_fft_size)),
+			 m_s2_input_im((float*)malloc(sizeof(float) * m_fft_size)),
+			 m_s1_output_re((float*)malloc(sizeof(float) * m_fft_size)),
+			 m_s1_output_im((float*)malloc(sizeof(float) * m_fft_size)),
+			 m_s2_output_re((float*)malloc(sizeof(float) * m_fft_size)),
+			 m_s2_output_im((float*)malloc(sizeof(float) * m_fft_size)),
+			 m_conv_output((float*)malloc(sizeof(float) * m_fft_size)),
+		 	 _fft(std::make_unique<fft_component>(m_fft_size))
 		{
 		}
 
@@ -27,20 +43,20 @@ namespace audio_analysis_lib
 
 		}
 
-		void sound_convolution_impl::frame_convolution(const float* src1, const float* src2)
+		float* sound_convolution_impl::frame_convolution(const float* src1, const float* src2)
 		{
-
+			return nullptr;
 		}
 	}
-	sound_convolution::sound_convolution(int init_fft_size)
-		:pImpl(std::make_unique<my_signal_processing::sound_convolution_impl>(init_fft_size))
+	sound_convolution::sound_convolution(int init_frame_size)
+		: pImpl(std::make_unique<my_signal_processing::sound_convolution_impl>(init_frame_size))
 	{
 	}
 
 	sound_convolution::~sound_convolution() { }
 
-	void sound_convolution::frame_convolution(const float* src1, const float* src2)
+	float* sound_convolution::frame_convolution(const float* src1, const float* src2)
 	{
-		pImpl->frame_convolution(src1, src2);
+		return pImpl->frame_convolution(src1, src2);
 	}
 }
--- src/sound_convolution.hpp
+++ src/sound_convolution.hpp
@@ -10,15 +10,15 @@ namespace audio_analysis_lib
 	}
 	namespace my_world_voice_feature
 	{
-		class fft_component;
+		class fft_component_impl;
 	}
 
 	class sound_convolution
 	{
 	public:
-		sound_convolution(int init_fft_size);
+		sound_convolution(int init_frame_size);
 		~sound_convolution();
-		void frame_convolution(const float* src1, const float* src2);
+		float* frame_convolution(const float* src1, const float* src2);
 
 	private:
 		std::unique_ptr<my_signal_processing::sound_convolution_impl> pImpl;
