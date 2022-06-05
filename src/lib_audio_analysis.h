f6ecfea36c5ea01c07789bc6b1eda8567aeab3da
tochinai <yoooomaruuuu@gmail.com>
2022-06-05 22:03:06 +0900

    header include追加忘れ修正

--- src/lib_audio_analysis.h
+++ src/lib_audio_analysis.h
@@ -1,11 +1,13 @@
 ﻿#pragma once
 // ヘッダインクルード
 #include "fft_component.hpp"
+#include "fft_exception.hpp"
 #include "window_functions.hpp"
 #include "world_harvest_f0.hpp"
 #include "world_cheaptrick_sp.hpp"
 #include "world_d4c_ap.hpp"
 #include "world_synthesis.hpp"
+#include "sound_convolution.hpp"
 #ifdef _WIN32
 #include "input_capture.hpp"
 #endif
